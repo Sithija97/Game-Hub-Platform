82e6980955e82f0a5bb9fc1007f22b44ea8e7ee5
Sithija Shehara <nsithijashehara@gmail.com>
2022-07-06 22:04:11 +0530

    initial app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [boardData, setBoardData] = useState(JSON.parse());
+  const [characterArray, setCharacterArray] = useState([]);
+  return <div></div>;
+}
+
+export default App;
