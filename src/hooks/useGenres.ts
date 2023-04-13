654cc2172fcad81fa525cf6503ace088894b600d
Sithija Shehara <nsithijashehara@gmail.com>
2023-04-13 12:17:24 +0530

    created a generic data fetching hook

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,36 +1,10 @@
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
+import useData from "./useData";
 
 export interface Genre {
   id: number;
   name: string;
 }
 
-interface GenreResponse {
-  count: number;
-  results: Genre[];
-}
-
-const useGenres = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    apiClient
-      .get<GenreResponse>("/genres")
-      .then((res) => {
-        setGenres(res.data.results);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setIsLoading(false);
-      });
-  }, []);
-
-  return { genres, error, isLoading };
-};
+const useGenres = () => useData<Genre>("/genres");
 
 export default useGenres;
